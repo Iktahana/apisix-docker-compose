1b9b1750d7d1ec3f971411ae9a85f02ac51919d5
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2024-11-23 08:18:55 +0900

    Switch to Debian Docker repository and remove sudo snap.
    Replaced the Ubuntu Docker repository with the Debian repository in install.sh for better compatibility. Also, removed unnecessary package upgrades and switched the yq installation method from snap to apt for consistency.

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt upgrade -y
-
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-# Install Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt update -y
 sudo apt install docker-ce -y
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo docker --version
 
 # Install yq
-sudo snap install yq
+sudo apt install yq
 
 # Generate random key function
 generate_random_key() {
