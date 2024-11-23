97bac7418ba1f958f974dd45323c38109f81dab1
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2024-11-23 22:58:32 +0900

    Update configs and add automation scripts
    Removed `requirement.txt` and added requirements to `requirements.txt` instead. Overhauled `apisix_conf/config.yaml` with updated settings for admin, Etcd, and Nginx configurations. Added `update_dasboard_conf.py` to automate configuration updates and expanded `install.sh` to include Docker operations.

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@ sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update -y
-sudo snap install docker -y
-sudo docker --version
 
-python update_dasboard_conf.py
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install python3 python3-pip -y
+
+sudo docker --version
+python3 --version
+python3 update_dasboard_conf.py
 
 docker compose up -d
 
-curl --request GET -sL \
-     --url 'http://localhost:80'
-curl curl --request GET -sL \
-     --url 'http://localhost:9000'
+curl --request GET -sL --url 'http://localhost:80'
+curl --request GET -sL --url 'http://localhost:9000'
