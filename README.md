274df5b904cdf0954683669bae6410de4fc2e9bc
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2024-11-23 21:56:14 +0900

    Remove configuration file conf/apisix/config.yaml
    Deleted the default configuration file in conf/apisix/config.yaml to manage custom settings through conf/config.yaml. This change helps in maintaining a cleaner and more manageable configuration setup by keeping custom configurations separate.

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 
 Add environment variables through cloud services, etc.
 
-
-
+```dotenv
+APISIX_ADMIN_KEY=X8T9Ad7okkxCK1EcrOUcH9tPiSvyEPId
+APISIX_ADMIN_PASSWORD=sZppxEQ6uXDNlvsF
+APISIX_ADMIN_USER=admin
+```
--- install.sh
+++ install.sh
@@ -8,20 +8,5 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update -y
-sudo apt install docker-ce -y
+sudo snap install docker -y
 sudo docker --version
-
-# Install yq
-sudo apt install yq
-
-# Generate random key function
-generate_random_key() {
-  openssl rand -base64 16
-}
-
-# Generate a new Admin Key for APISIX
-admin_key=$(generate_random_key)
-
-# Modify keys in YAML file
-yq eval '.deployment.admin.admin_key[0].key = env(admin_key)' -i conf/apisix/config.yaml
-echo "Generated admin_key: $admin_key"
